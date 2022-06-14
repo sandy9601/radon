e67d133b0c8ccf576dfdb8d3916ee9800f211875
sandy9601 <darshanamsandeep375@gmail.com>
2022-06-14 15:58:24 +0530

    incomplete

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -7,11 +7,13 @@ const userModel = require("../models/userModel")
 
 const createorder= async function (req, res) {
     let data= req.body
+    //user validation
     let user_id=data.userid
     let userid=await userModel.findById({_id:user_id})
     if(!userid){
         res.send("used id nont present")
     }
+    //product validation
     let product_id=data.productid
     console.log(product_id)
     let productid=await productmodel.findById({_id:product_id})
